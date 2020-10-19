ee15792924510711bffb4238e26ddbbcf05f2725
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2020-10-19 21:07:29 +0200

    Added comments to increase clarity

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ const (
 	REGION      = "eu-central-1"
 
 	FILE      = "video.mp4"
-	PART_SIZE = 6_000_000
+	PART_SIZE = 6_000_000 // Has to be 5_000_000 minimim
 	RETRIES   = 2
 )
 
@@ -31,15 +31,19 @@ func init() {
 }
 
 func main() {
+  // Open the file
 	file, _ := os.Open(FILE)
 	defer file.Close()
 
+  // Get file size
 	stats, _ := file.Stat()
 	fileSize := stats.Size()
 
+  // put file in byteArray
 	buffer := make([]byte, fileSize)
 	file.Read(buffer)
 
+  // Create MultipartUpload object
 	expiryDate := time.Now().AddDate(0, 0, 1)
 	createdResp, err := s3session.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
 		Bucket:  aws.String(BUCKET_NAME),
@@ -56,6 +60,7 @@ func main() {
 	var remaining = int(fileSize)
 	var partNum = 1
 	var completedParts []*s3.CompletedPart
+  // Loop till remaining upload size is 0
 	for start = 0; remaining != 0; start += PART_SIZE {
 		if remaining < PART_SIZE {
 			currentSize = remaining
@@ -64,6 +69,7 @@ func main() {
 		}
 
 		completed, err := Upload(createdResp, buffer[start:start+currentSize], partNum)
+    // If upload function failed (meaning it retried acoording to RETRIES)
 		if err != nil {
 			_, err = s3session.AbortMultipartUpload(&s3.AbortMultipartUploadInput{
 				Bucket:   createdResp.Bucket,
@@ -71,18 +77,23 @@ func main() {
 				UploadId: createdResp.UploadId,
 			})
 			if err != nil {
+        // god speed
 				fmt.Println(err)
 				return
 			}
 		}
 
+    // Detract the current part size from remaining
 		remaining -= currentSize
 		fmt.Printf("Part %v complete, %v btyes remaining\n", partNum, remaining)
 
-		partNum++
+    // Add the completed part to our list
 		completedParts = append(completedParts, completed)
+    partNum++
+
 	}
 
+  // All the parts are uploaded, completing the upload
 	resp, err := s3session.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
 		Bucket:   createdResp.Bucket,
 		Key:      createdResp.Key,
@@ -99,6 +110,7 @@ func main() {
 
 }
 
+// Uploads the fileBytes bytearray a MultiPart upload
 func Upload(resp *s3.CreateMultipartUploadOutput, fileBytes []byte, partNum int) (completedPart *s3.CompletedPart, err error) {
 	var try int
 	for try <= RETRIES {
@@ -110,14 +122,18 @@ func Upload(resp *s3.CreateMultipartUploadOutput, fileBytes []byte, partNum int)
 			UploadId:      resp.UploadId,
 			ContentLength: aws.Int64(int64(len(fileBytes))),
 		})
+    // Upload failed
 		if err != nil {
 			fmt.Println(err)
+      // Max retries reached! Quitting
 			if try == RETRIES {
 				return nil, err
 			} else {
+        // Retrying
 				try++
 			}
 		} else {
+      // Upload is done!
 			return &s3.CompletedPart{
 				ETag:       uploadResp.ETag,
 				PartNumber: aws.Int64(int64(partNum)),
